80fe6bda48e71f1e52116b2104377e9b20e1b321
AndrewsNick-FS <148885746+AndrewsNick-FS@users.noreply.github.com>
2024-11-05 15:03:37 -0600

    started adding routers to the app.js module

---
+++ app/routes/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "From the API" });
+});
+
+router.post("/", (req, res) => {
+  console.log("Request body >>>", req.body);
+  const { data } = req.body;
+  res.status(200).json({ message: "We got the request", data: data });
+});
+
+module.exports = router;
