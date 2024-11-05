def79d5d61f11792618fbdd5f50dce9645bc819c
AndrewsNick-FS <148885746+AndrewsNick-FS@users.noreply.github.com>
2024-11-05 14:20:59 -0600

    Removed folders and updated routes, fixed missing import for the server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 require("dotenv").config();
-const app = require("./Week1/app/app");
+const app = require("./app/app");
 
-http.createServer().listen(process.env.PORT, () => {
+http.createServer(app).listen(process.env.PORT, () => {
   console.log("server is running on port", process.env.PORT);
 });
