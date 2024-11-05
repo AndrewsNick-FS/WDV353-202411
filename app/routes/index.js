38551489e66b3ea70f6c948cbbafa9f626f2fb9d
AndrewsNick-FS <148885746+AndrewsNick-FS@users.noreply.github.com>
2024-11-05 15:34:00 -0600

    created the delete by id function, as well as the error callbacks.

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,13 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "From the API" });
 });
 
+router.delete("/:id", (req, res) => {
+  const todo = arr.find((obj) => obj.id === id);
+  const id = req.params.id;
+  console.log("params >>>", id);
+  res.status(200).json({ message: "From the API" });
+});
+
 router.post("/", (req, res) => {
   console.log("Request body >>>", req.body);
   const { data } = req.body;
