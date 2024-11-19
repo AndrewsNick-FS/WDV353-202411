bec8c5cd9c1744965ed7749f9b5b016224f44c49
AndrewsNick-FS <148885746+AndrewsNick-FS@users.noreply.github.com>
2024-11-19 11:13:30 -0600

    moved the schemas into the proper folders, accidentally started them in the wrong ones

--- app/controllers/models.js
+++ app/controllers/models.js
@@ -1,10 +1,1 @@
-const mongoose = require("mongoose");
 
-const modelSchema = new mongoose.Schema({
-  name: { type: String, required: true }, // e.g., Corolla, Mustang
-  type: { type: String, required: true }, // e.g., Sedan, SUV
-  price: { type: Number, required: true }, // e.g., 25000
-  car: { type: mongoose.Schema.Types.ObjectId, ref: "Car", required: true }, // Reference to Car
-});
-
-module.exports = mongoose.model("Model", modelSchema);
