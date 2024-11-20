5f70ab8163947fa1237739ed0307b1c57ab9b12f
AndrewsNick-FS <148885746+AndrewsNick-FS@users.noreply.github.com>
2024-11-19 21:17:54 -0600

    created the api.js file in services to connect the frontend and backend "ids" together

---
+++ app/car-model-app/src/services/api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:3000/api";
+
+export const getAllCars = async () => axios.get(`${API_BASE_URL}/cars`);
+export const getCarsById = async (id) =>
+  axios.get(`${API_BASE_URL}/cars/${id}`);
+export const createCar = async (data) =>
+  axios.post(`${API_BASE_URL}/cars`, data);
+export const updateCar = async (id, data) =>
+  axios.put(`${API_BASE_URL}/cars/${id}`);
+export const deleteCar = async (id) =>
+  axios.delete(`${API_BASE_URL}/cars/${id}`);
+
+export const getAllModels = async () => axios.get(`${API_BASE_URL}/models`);
+export const getModelById = async (id) =>
+  axios.get(`${API_BASE_URL}/models/${id}`);
+export const createModel = async (data) =>
+  axios.post(`${API_BASE_URL}/models`, data);
+export const updateModel = async (id, data) =>
+  axios.put(`${API_BASE_URL}/models/${id}`);
+export const deleteModel = async (id) =>
+  axios.delete(`${API_BASE_URL}/models/${id}`);
