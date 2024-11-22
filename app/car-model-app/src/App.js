73e9faa82aa7bc407d35e304cfbb83b403bc4a92
AndrewsNick-FS <148885746+AndrewsNick-FS@users.noreply.github.com>
2024-11-21 22:56:53 -0600

    Installed axios and tailwind, for some light beautifications. Built the Car form and and list, had to swap them around. Also tweaked the tailwind app for easier use.

--- app/car-model-app/src/App.js
+++ app/car-model-app/src/App.js
@@ -4,12 +4,20 @@ import CarList from "./components/CarList";
 import ModelList from "./components/ModelList";
 
 const App = () => {
-  <Router>
-    <Routes>
-      <Route path="/cars" element={<CarList />} />
-      <Route path="/models" element={<ModelList />} />
-    </Routes>
-  </Router>;
+  return (
+    <div className="min-h-screen bg-black text-cyan p-4">
+      <header className="text-center text-2x1 font-bold p-4">
+        Car Management
+      </header>
+      <main className="max-w-4x1 mx-auto p-6 rounded-lg bg-gray-90 shadow-lg">
+        <Router>
+          <Routes>
+            <Route path="/cars" element={<CarList />} />
+            <Route path="/models" element={<ModelList />} />
+          </Routes>
+        </Router>
+      </main>
+    </div>
+  );
 };
-
 export default App;
